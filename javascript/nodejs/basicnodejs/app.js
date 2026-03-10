f896850b50515fb1af9ff11a72de4758d2dace1d
Darshan B <darshandarshan.cr07@gmail.com>
2026-03-11 01:31:14 +0530

    Started NodeJS

---
+++ javascript/nodejs/basicnodejs/app.js
@@ -0,0 +1,5 @@
+const math = require('./math')
+
+console.log(math.add(10,5))
+console.log(math.subtract(10,5))
+console.log(math.multiply(10,5))
---
+++ javascript/nodejs/basicnodejs/pathmodule.js
@@ -0,0 +1,7 @@
+const path= require('path');
+
+const filePath = path.join(__dirname,'files','example.txt');
+console.log(__dirname)
+console.log(filePath)
+
+console.log(path.basename(filePath))
